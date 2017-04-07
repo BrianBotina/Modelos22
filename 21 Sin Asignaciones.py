95603bb0b08495b4b8e7488864cc53e2879e3b64
BrianBotina <brayan_botina96@hotmail.com>
2017-04-07 16:42:24 -0500

    Add files via upload

---
+++ 21 Sin Asignaciones.py
@@ -0,0 +1,66 @@
+import random
+def crear_baraja():
+    return random.sample([x for x in [2,3,4,5,6,7,8,9,'J','Q','K','A']],12)
+
+def jugar(jugador,mesa,maso):
+    if maso == []:
+        print(jugador)
+        print(mesa)
+        return []
+    else:
+        print(jugador)
+        print(mesa[0:(len(mesa)-1)])
+        if puntosJugador(jugador,0) == 21 and puntosMesa(mesa,0) != 21:
+            print("Felicidades, Has ganado!")
+            exit()
+        elif puntosJugador(jugador,0) > 21:
+            print("Lo siento, Has perdido!")
+            print("Puntos Jugador: ",puntosJugador(jugador,0))
+            exit()
+        elif puntosMesa(mesa,0) > 21 and puntosJugador(jugador,0) <= 21:
+            print("La mesa ha perdido, Ganaste!")
+            print("Puntos Mesa: ",puntosMesa(mesa,0))
+            exit()
+        if puntosJugador(jugador,0) < 21:
+            print("LLevas: ",puntosJugador(jugador,0)," puntos")
+            if input("Deseas continuar? s/n:") == 's':
+                jugar(jugador+[maso[0]], mesa+[maso[1]], maso[2:])
+            else:
+                if puntosJugador(jugador,0) > puntosMesa(mesa,0) and puntosJugador(jugador,0) <=21:
+                    print("Felicidades has Ganado!")
+                    print("Puntos Jugador: ", puntosJugador(jugador,0))
+                    print("Puntos Casa: ", puntosMesa(mesa,0))
+                    exit()
+                elif puntosJugador(jugador,0) < puntosMesa(mesa,0) and puntosMesa(mesa,0) <=21:
+                    print("Lo siento has perdido!")
+                    print("Puntos Jugador: ", puntosJugador(jugador,0))
+                    print("Puntos Casa: ", puntosMesa(mesa,0))
+                    exit()
+
+def puntosJugador(jugador, resultado):
+    if jugador == []:
+        return resultado
+    else:
+        if jugador[0] == 'J' or jugador[0] == 'Q' or jugador[0] == 'K':
+            return puntosJugador(jugador[1:], resultado+10)
+        elif jugador[0] == 'A' and resultado >10:
+            return puntosJugador(jugador[1:], resultado+1)
+        elif jugador[0] == 'A' and resultado <=10:
+            return puntosJugador(jugador[1:], resultado+10)
+        else:
+            return puntosJugador(jugador[1:], resultado+jugador[0])
+        
+def puntosMesa(mesa, resultado):
+    if mesa == []:
+        return resultado
+    else:
+        if mesa[0] == 'J' or mesa[0] == 'Q' or mesa[0] == 'K':
+            return puntosMesa(mesa[1:], resultado+10)
+        elif mesa[0] == 'A' and resultado >10:
+            return puntosMesa(mesa[1:], resultado+1)
+        elif mesa[0] == 'A' and resultado <=10:
+            return puntosMesa(mesa[1:], resultado+10)
+        else:
+            return puntosMesa(mesa[1:], resultado+mesa[0])
+
+print(jugar([],[],crear_baraja()))
