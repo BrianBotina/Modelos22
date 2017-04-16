a3f12db99c8e442f1545698db14dbb661cfdbb6a
BrianBotina <brayan_botina96@hotmail.com>
2017-04-15 21:40:16 -0500

    Update 21 Sin Asignaciones.py

--- 21 Sin Asignaciones.py
+++ 21 Sin Asignaciones.py
@@ -3,39 +3,56 @@ def crear_baraja():
     return random.sample([x for x in [2,3,4,5,6,7,8,9,'J','Q','K','A']],12)
 
 def jugar(jugador,mesa,maso):
-    if maso == []:
-        print(jugador)
-        print(mesa)
-        return []
-    else:
-        print(jugador)
-        print(mesa[0:(len(mesa)-1)])
-        if puntosJugador(jugador,0) == 21 and puntosMesa(mesa,0) != 21:
-            print("Felicidades, Has ganado!")
-            exit()
-        elif puntosJugador(jugador,0) > 21:
-            print("Lo siento, Has perdido!")
-            print("Puntos Jugador: ",puntosJugador(jugador,0))
-            exit()
-        elif puntosMesa(mesa,0) > 21 and puntosJugador(jugador,0) <= 21:
-            print("La mesa ha perdido, Ganaste!")
-            print("Puntos Mesa: ",puntosMesa(mesa,0))
-            exit()
-        if puntosJugador(jugador,0) < 21:
-            print("LLevas: ",puntosJugador(jugador,0)," puntos")
-            if input("Deseas continuar? s/n:") == 's':
-                jugar(jugador+[maso[0]], mesa+[maso[1]], maso[2:])
-            else:
-                if puntosJugador(jugador,0) > puntosMesa(mesa,0) and puntosJugador(jugador,0) <=21:
-                    print("Felicidades has Ganado!")
-                    print("Puntos Jugador: ", puntosJugador(jugador,0))
-                    print("Puntos Casa: ", puntosMesa(mesa,0))
-                    exit()
-                elif puntosJugador(jugador,0) < puntosMesa(mesa,0) and puntosMesa(mesa,0) <=21:
-                    print("Lo siento has perdido!")
-                    print("Puntos Jugador: ", puntosJugador(jugador,0))
-                    print("Puntos Casa: ", puntosMesa(mesa,0))
-                    exit()
+    if len(jugador)<2 and len(mesa)<2:
+        jugar(jugador+[maso[0]], mesa+[maso[1]], maso[2:])
+    print(jugador)
+    print(mesa[:(len(mesa)-1)])
+    if puntosJugador(jugador,0) == 21 and puntosMesa(mesa,0) != 21:
+        print("Felicidades, Has ganado!")
+        print("Cartas Jugador: ",jugador)
+        print("Cartas Mesa: ",mesa)
+        exit()
+    elif puntosMesa(mesa,0) == 21 and puntosJugador(jugador,0) != 21:
+        print("Perdiste, La mesa ha llegado a 21!")
+        print("Cartas Jugador: ",jugador)
+        print("Cartas Mesa: ",mesa)
+        exit()
+    elif puntosJugador(jugador,0) > 21:
+        print("Lo siento, Has perdido!")
+        print("Puntos Jugador: ",puntosJugador(jugador,0))
+        print("Cartas Jugador: ",jugador)
+        print("Cartas Mesa: ",mesa)
+        exit()
+    elif puntosMesa(mesa,0) > 21:
+        print("La mesa ha perdido, Ganaste!")
+        print("Puntos Mesa: ",puntosMesa(mesa,0))
+        print("Cartas Jugador: ",jugador)
+        print("Cartas Mesa: ",mesa)
+        exit()
+    if puntosJugador(jugador,0) < 21:
+        print("LLevas: ",puntosJugador(jugador,0)," puntos")
+        if input("Deseas continuar? s/n: ") == 's':
+            jugar(jugador+[maso[0]], mesa+[maso[1]], maso[2:])
+        else:
+            print("Has plantado en: ",puntosJugador(jugador,0))
+            if puntosJugador(jugador,0) > puntosMesa(mesa, 0) and puntosJugador(jugador,0) < 21:
+                print("Felicidades, Has ganado!")
+                print("Puntos Mesa: ",puntosMesa(mesa, 0))
+                print("Cartas Jugador: ",jugador)
+                print("Cartas Mesa: ",mesa)
+                exit()
+            if puntosJugador(jugador,0) < puntosMesa(mesa, 0) and puntosMesa(mesa,0) < 21:
+                print("Lo siento, has Perdido!")
+                print("Puntos Mesa: ",puntosMesa(mesa, 0))
+                print("Cartas Jugador: ",jugador)
+                print("Cartas Mesa: ",mesa)
+                exit()
+            if puntosJugador(jugador,0) == puntosMesa(mesa, 0) and puntosMesa(mesa,0) < 21:
+                print("Lo siento, has Perdido!")
+                print("Puntos Mesa: ",puntosMesa(mesa, 0))
+                print("Cartas Jugador: ",jugador)
+                print("Cartas Mesa: ",mesa)
+                exit() 
 
 def puntosJugador(jugador, resultado):
     if jugador == []:
